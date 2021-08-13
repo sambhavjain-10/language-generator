d2c3be4cba51f2c758f0df6231e19376be339e7c
Ashis-007 <ashisjc@gmail.com>
2021-08-13 20:42:12 +0530

    Add copy to clipboard, enter to submit

--- src/App.js
+++ src/App.js
@@ -1,67 +1,72 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 import Word from "./components/Word";
 
 const URL =
-    "https://translated-mymemory---translation-memory.p.rapidapi.com/api/get";
+	"https://translated-mymemory---translation-memory.p.rapidapi.com/api/get";
 
 const headers = {
-    "x-rapidapi-key": "c330eef171mshdeea84f19efe738p16b4c9jsn717f1da7d399",
-    "x-rapidapi-host":
-        "translated-mymemory---translation-memory.p.rapidapi.com",
+	"x-rapidapi-key": "c330eef171mshdeea84f19efe738p16b4c9jsn717f1da7d399",
+	"x-rapidapi-host": "translated-mymemory---translation-memory.p.rapidapi.com",
 };
 
 const App = () => {
-    const [input, setInput] = useState("");
-    const [generated, setGenerated] = useState([]);
+	const [input, setInput] = useState("");
+	const [generated, setGenerated] = useState([]);
 
-    const translate = () => {
-        setGenerated([]);
-        const array = input.split(",");
+	const translate = (e) => {
+		e.preventDefault();
+		setGenerated([]);
+		const array = input.split(",");
 
-        array.map(async word => {
-            let { data: french } = await axios.get(URL, {
-                headers,
-                params: { q: word.trim(), langpair: "en|fr" },
-            });
-            let { data: spanish } = await axios.get(URL, {
-                headers,
-                params: { q: word.trim(), langpair: "en|es" },
-            });
+		array.map(async (word) => {
+			let { data: french } = await axios.get(URL, {
+				headers,
+				params: { q: word.trim(), langpair: "en|fr" },
+			});
+			let { data: spanish } = await axios.get(URL, {
+				headers,
+				params: { q: word.trim(), langpair: "en|es" },
+			});
 
-            setGenerated(prev => [
-                ...prev,
-                {
-                    english: word.trim(),
-                    french: french.responseData.translatedText,
-                    spanish: spanish.responseData.translatedText,
-                },
-            ]);
-        });
-    };
+			setGenerated((prev) => [
+				...prev,
+				{
+					english: word.trim(),
+					french: french.responseData.translatedText,
+					spanish: spanish.responseData.translatedText,
+				},
+			]);
+		});
+	};
 
-    return (
-        <div className="app">
-            <input
-                name="input"
-                value={input}
-                onChange={e => setInput(e.target.value)}
-            />
-            <button onClick={translate}>RUN</button>
-            <br />
-            <div>
-                {generated.length > 0 &&
-                    generated.map(i => (
-                        <Word
-                            english={i.english}
-                            french={i.french}
-                            spanish={i.spanish}
-                        />
-                    ))}
-            </div>
-        </div>
-    );
+	useEffect(() => {}, [generated]);
+
+	return (
+		<div className="app">
+			<form onSubmit={translate}>
+				<input
+					name="input"
+					value={input}
+					onChange={(e) => setInput(e.target.value)}
+				/>
+				<button type="submit">RUN</button>
+			</form>
+			<br />
+			<div>
+				{generated.length > 0 &&
+					generated.map((i, index) => (
+						<Word
+							english={i.english}
+							french={i.french}
+							spanish={i.spanish}
+							key={index}
+						/>
+					))}
+			</div>
+		</div>
+	);
 };
 
 export default App;
--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Word = ({ english, spanish, french }) => {
-    return (
-        <p>
-            {english.toUpperCase().replace(" ", "_")}:{"{"}
-            <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ENGLISH: {`"${english}"`},<br />
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;FRENCH: {`"${french}"`}, <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SPANISH: {`"${spanish}"`},<br />{" "}
-            {"},"}
-            <br />
-        </p>
-    );
+	const word = `${english.toUpperCase().replace(" ", "_")}: {
+    ENGLISH: "${english}",
+    FRENCH: "${french}",
+    SPANISH: "${spanish}"
+}`;
+	useEffect(() => {
+		navigator.clipboard.writeText(word);
+	}, []);
+
+	return (
+		<div style={{ whiteSpace: "pre" }}>
+			{/* {english.toUpperCase().replace(" ", "_")}:{"{"}
+			<br />
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ENGLISH: {`"${english}"`},<br />
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;FRENCH: {`"${french}"`}, <br />
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SPANISH: {`"${spanish}"`},<br />{" "}
+			{"},"}
+			<br /> */}
+			{word}
+		</div>
+	);
 };
 
 export default Word;
